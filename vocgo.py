8a925f9a5cbc98cf3eab74c0d6a157cd43e99879
Lori Corbani <Lori.Corbani@jax.org>
2006-10-19 14:42:02 +0000

    TR 6812

--- vocgo.py
+++ vocgo.py
@@ -38,19 +38,20 @@ def createBCPfile():
 
 	cacheBCP = open(outDir + '/%s.bcp' % (table), 'w')
 
-	results = db.sql('select t._Term_key, vd._DAG_key, t.term, t.sequenceNum, a.accID, d.abbreviation ' + \
-		'from VOC_Term t, ACC_Accession a, VOC_VocabDAG vd, DAG_DAG d ' + \
+	results = db.sql('select t._Term_key, n._DAG_key, t.term, a.accID, d.abbreviation ' + \
+		'from VOC_Term t, ACC_Accession a, VOC_VocabDAG vd, DAG_Node n, DAG_DAG d ' + \
 		'where t._Vocab_key = 4 ' + \
 		'and t._Term_key = a._Object_key ' + \
 		'and a._MGIType_key = 13 ' + \
 		'and a.preferred = 1 ' + \
 		'and t._Vocab_key = vd._Vocab_key ' + \
-		'and vd._DAG_key = d._DAG_key ', 'auto')
+		'and t._Term_key = n._Object_key ' + \
+		'and vd._DAG_key = n._DAG_key ' + \
+		'and n._DAG_key = d._DAG_key ', 'auto')
 
 	for r in results:
 	    cacheBCP.write(mgi_utils.prvalue(r['_Term_key']) + COLDL + \
 	    		   mgi_utils.prvalue(r['_DAG_key']) + COLDL + \
-	    		   mgi_utils.prvalue(r['sequenceNum']) + COLDL + \
 	    		   mgi_utils.prvalue(r['abbreviation']) + COLDL + \
 	    		   mgi_utils.prvalue(r['accID']) + COLDL + \
 	    		   mgi_utils.prvalue(r['term']) + LINEDL)
