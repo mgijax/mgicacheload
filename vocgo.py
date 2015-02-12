5b38b7eee2fa31d6ac126d2ed528dc00cb5c1655
Lori Corbani <Lori.Corbani@jax.org>
2015-02-12 15:03:16 +0000

    mgicacheload-5-2-1-4/TR11750

--- vocgo.py
+++ vocgo.py
@@ -47,16 +47,17 @@ def createBCPfile():
 
 	cacheBCP = open(outDir + '/%s.bcp' % (table), 'w')
 
-	results = db.sql('select t._Term_key, n._DAG_key, t.term, a.accID, d.abbreviation ' + \
-		'from VOC_Term t, ACC_Accession a, VOC_VocabDAG vd, DAG_Node n, DAG_DAG d ' + \
-		'where t._Vocab_key = 4 ' + \
-		'and t._Term_key = a._Object_key ' + \
-		'and a._MGIType_key = 13 ' + \
-		'and a.preferred = 1 ' + \
-		'and t._Vocab_key = vd._Vocab_key ' + \
-		'and t._Term_key = n._Object_key ' + \
-		'and vd._DAG_key = n._DAG_key ' + \
-		'and n._DAG_key = d._DAG_key ', 'auto')
+	results = db.sql('''select t._Term_key, n._DAG_key, t.term, a.accID, d.abbreviation 
+		from VOC_Term t, ACC_Accession a, VOC_VocabDAG vd, DAG_Node n, DAG_DAG d 
+		where t._Vocab_key = 4 
+		and t._Term_key = a._Object_key 
+		and a._MGIType_key = 13 
+		and a.preferred = 1 
+		and t._Vocab_key = vd._Vocab_key 
+		and t._Term_key = n._Object_key 
+		and vd._DAG_key = n._DAG_key 
+		and n._DAG_key = d._DAG_key 
+		''', 'auto')
 
 	for r in results:
 	    cacheBCP.write(mgi_utils.prvalue(r['_Term_key']) + COLDL + \
@@ -72,7 +73,6 @@ def createBCPfile():
 
 print '%s' % mgi_utils.date()
 db.useOneConnection(1)
-#db.set_sqlLogFunction(db.sqlLogAll)
 createBCPfile()
 db.useOneConnection(0)
 print '%s' % mgi_utils.date()
