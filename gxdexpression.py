7d1c3931a8d206f1c6245d4bdc0ab00b3ba7c485
Lori Corbani <lori.corbani@jax.org>
2024-02-07 08:00:47 -0500

    wts2-1399/fl2-736/Null in result note field of result summary.

--- gxdexpression.py
+++ gxdexpression.py
@@ -375,7 +375,7 @@ def generateCacheResults(isFull, dbResultGroups, assayResultMap):
                 hasimage = computeHasImage(allResultsForAssay)
 
                 # check specimen key
-                resultNote = ''
+                resultNote = ""
 
                 try:
                     _specimen_key = rep['_specimen_key']
